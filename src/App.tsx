052f75318b155e880a12acc4da747fb6a55f87df
noahpro <noahpro@gmail.com>
2023-11-20 19:56:21 -0500

    style!!!
    Co-authored-by: GideonC <TheLumpiest@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,19 @@
 import * as React from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import { CounterButtons } from "./Counter";
-import TeamSelector from "./TeamSelector";
-import Chooser from "./Chooser";
-import UseSwitchesBasic from "./CustomSwitch";
-import { TextField } from "@mui/material";
-import { MouseEvent } from "react";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
-
-enum Alliance {
-  RED,
-  BLUE,
-}
+import AutonomousForm from "./AutonomousForm";
 
 export default function App() {
-  const [alliance, setAlliance] = React.useState(Alliance.RED);
-  const ButtonComponent = () => {
-    const handleMouseEvent = (e: MouseEvent<HTMLButtonElement>) => {
-      setAlliance(alliance == Alliance.RED ? Alliance.BLUE : Alliance.RED);
-      // Do something
-    };
+  const [formType, setFormType] = React.useState("Autonomous");
 
-    return (
-      <button onClick={handleMouseEvent}>
-        {" "}
-        <UseSwitchesBasic />
-      </button>
-    );
-  };
   return (
-    <div
-      className={`min-h-screen w-screen flex flex-col items-center
-      ${alliance == Alliance.RED ? "bg-red-400" : "bg-blue-500"}`}
-    >
-      <div className="p-4 pt-10">
-        <Typography variant="h4" component="h1" gutterBottom>
-          Experimental Form
-        </Typography>
-        <div className="flex flex-row items-center justify-center">
-          <div className="justify-center">Alliance</div>
-          <UseSwitchesBasic />
-          <ButtonComponent />
-        </div>
-        <TeamSelector />
-        <Typography>Alliance</Typography>
-        <Typography variant="h4"> Position</Typography>
-        <Chooser value1="Red1" value2="Red2" value3="Red3" />
-        <CounterButtons />
-        <CounterButtons />
-        <TextField
-          id="outlined-textarea"
-          label="Comments"
-          placeholder="Null"
-          multiline
-        />
+    <div>
+      <div className="flex flex-row grid-1">
+        <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setFormType("Autonomous")}>Autonomous</button>
+        <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setFormType("Teleop")}>Teleop</button>
       </div>
+      {formType == "Autonomous" && <AutonomousForm />}
+
     </div>
-  );
+  )
 }
